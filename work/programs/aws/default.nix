73833744ba8d5091ad83323e126112d28c906ce5
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-18 16:47:22 +0100

    Install awsp

--- work/programs/aws/default.nix
+++ work/programs/aws/default.nix
@@ -1,18 +1,21 @@
 { pkgs, config, ... }:
 let
   nix-dss = import
-    (builtins.fetchGit {
-      url = "git@github.bamtech.co:jkozlowski/nix-dss";
-      ref = "v0.0.5";
-      rev = "6557306249078fc8f0f41f30821bb45ef3e0dc4c";
-    })
+    (
+      builtins.fetchGit {
+        url = "git@github.bamtech.co:jkozlowski/nix-dss";
+        ref = "v0.0.5";
+        rev = "6557306249078fc8f0f41f30821bb45ef3e0dc4c";
+      }
+    )
     { inherit pkgs; };
 in
 {
   home.packages = [
     pkgs.awscli
     pkgs.ssm-session-manager-plugin
-    (pkgs.callPackage ./ssm-helpers.nix { })
+    (pkgs.callPackage ./ssm-helpers.nix {})
+    (pkgs.callPackage ./awsp.nix {})
     nix-dss.bamc
   ];
 
