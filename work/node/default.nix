717744f3f3954b01afd5337f526a798741303824
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-25 13:51:31 +0100

    Fix bash

--- work/node/default.nix
+++ work/node/default.nix
@@ -5,7 +5,7 @@ let
   dynamodb-admin-local =
     pkgs.writeScriptBin
       "dynamodb-admin"
-      "DYNAMO_ENDPOINT=http://localhost:4569 ${dynamodb-admin}/bin/dynamodb-admin $@";
+      "DYNAMO_ENDPOINT=http://localhost:4569 ${dynamodb-admin}/bin/dynamodb-admin \"$@\"";
 in
 {
   home.packages = [ dynamodb-admin-local ];
