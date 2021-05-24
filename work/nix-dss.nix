695f152eeb7cbb9b85af3ac4cfa77970a20c8732
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-24 19:42:02 +0200

    Update a bunch of stuff, install scala3-repl

--- work/nix-dss.nix
+++ work/nix-dss.nix
@@ -1,7 +1,7 @@
 import (
   builtins.fetchGit {
     url = "git@github.bamtech.co:jkozlowski/nix-dss";
-    ref = "v0.0.11";
-    rev = "34ed8f84894d645fc25a04371a5338c9a1c93298";
+    ref = "v0.0.12";
+    rev = "e72b190d91a34e4a7280901f59bd520cf719b920";
   }
 )
