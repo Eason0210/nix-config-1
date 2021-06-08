7c3aafc3ca662624265c7565ea0229abcef302d0
Jakub Kozłowski <kubukoz@gmail.com>
2021-06-08 22:54:47 +0200

    Bump nix-dss

--- work/nix-dss.nix
+++ work/nix-dss.nix
@@ -1,7 +1,7 @@
 import (
   builtins.fetchGit {
     url = "git@github.bamtech.co:jkozlowski/nix-dss";
-    ref = "v0.0.12";
-    rev = "e72b190d91a34e4a7280901f59bd520cf719b920";
+    ref = "v0.0.13";
+    rev = "18b52ca2c3b434ccf27f69d8cc4d40241730b558";
   }
 )
