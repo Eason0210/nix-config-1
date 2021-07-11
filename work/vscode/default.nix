6d9c298f81e0a71c4392b5c6c7733c31af9afdda
Jakub Kozłowski <kubukoz@gmail.com>
2021-07-11 18:08:15 +0200

    Add work extensions to managed, update them

--- work/vscode/default.nix
+++ work/vscode/default.nix
@@ -2,28 +2,16 @@
 let
   nix-dss = import ../nix-dss.nix { inherit pkgs; };
   vscode-lib = import ../../vscode/lib.nix;
-  inherit (vscode-lib) configuredExtension;
-  extensionFromVscodeMarketplace = pkgs.vscode-utils.extensionFromVscodeMarketplace;
-  swagger-viewer = extensionFromVscodeMarketplace {
-    name = "swagger-viewer";
-    publisher = "Arjun";
-    version = "3.0.1";
-    sha256 = "01cz9ag06h0624v963ypnaahrpfzl0gv0nnw356llbv1pz90f6w5";
+  inherit (vscode-lib) configuredExtension managedPackages;
+  managed = managedPackages {
+    file = ./extensions/managed.nix;
+    inherit (pkgs) vscode-utils;
   };
-  sqltools = extensionFromVscodeMarketplace {
-    name = "sqltools";
-    publisher = "mtxr";
-    version = "0.23.0";
-    sha256 = "0gkm1m7jss25y2p2h6acm8awbchyrsqfhmbg70jaafr1dfxkzfir";
-  };
+  swagger-viewer = managed.Arjun.swagger-viewer;
+  sqltools = managed.mtxr.sqltools;
   snowflake =
     configuredExtension {
-      extension = extensionFromVscodeMarketplace {
-        name = "snowflake-driver-for-sqltools";
-        publisher = "koszti";
-        version = "0.4.1";
-        sha256 = "06csh4c9wjp6lvcvn915sjk9f1q6fg3kdsl3f94zqz8va11bcrbl";
-      };
+      extension = managed.koszti.snowflake-driver-for-sqltools;
       settings = {
         "sqltools.connections" = [
           (import ./semisecret-snowflake.nix)
---
+++ work/vscode/extensions/managed.nix
@@ -0,0 +1,25 @@
+#
+# This file was written by hmm (Home Manager Manager).
+# Don't edit it manually!
+# This file should always parse as an array of attrsets.
+#
+[
+  {
+    name = "swagger-viewer";
+    publisher = "Arjun";
+    version = "3.1.2";
+    sha256 = "1cjvc99x1q5w3i2vnbxrsl5a1dr9gb3s6s9lnwn6mq5db6iz1nlm";
+  }
+  {
+    name = "sqltools";
+    publisher = "mtxr";
+    version = "0.23.0";
+    sha256 = "0gkm1m7jss25y2p2h6acm8awbchyrsqfhmbg70jaafr1dfxkzfir";
+  }
+  {
+    name = "snowflake-driver-for-sqltools";
+    publisher = "koszti";
+    version = "0.4.2";
+    sha256 = "1wl7dg6ldvvi1r8jkry2j1ha3is57xf5dganlsk7i7yqpfxagckl";
+  }
+]
