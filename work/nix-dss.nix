cb3f06e54f8a695a021f992533c62cd4b29e3848
Jakub Kozłowski <kubukoz@gmail.com>
2021-08-24 21:29:11 +0200

    foo

--- work/nix-dss.nix
+++ work/nix-dss.nix
@@ -2,6 +2,6 @@ import (
   builtins.fetchGit {
     url = "git@github.bamtech.co:jkozlowski/nix-dss";
     ref = "master";
-    rev = "1655236f9b1323cd8c5e9fc2a777884577c7bdf3";
+    rev = "0b1074cd8fe6b3d1346424489e7527211b3d3fea";
   }
 )
