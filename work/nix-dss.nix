7c44d81270c66b3201f2d35aebfdcf6f607a48ad
Jakub Kozłowski <kubukoz@gmail.com>
2021-09-16 16:53:33 +0200

    Update nix-dss

--- work/nix-dss.nix
+++ work/nix-dss.nix
@@ -5,7 +5,7 @@ _: super:
       builtins.fetchGit {
         url = "git@github.bamtech.co:jkozlowski/nix-dss";
         ref = "master";
-        rev = "047987eddbaa732e0d33d1a3d7a014293352a4c2";
+        rev = "aef3ab6df06329d5dbc78d48a7065f7a3a0f01aa";
       }
     ) { pkgs = super; };
 }
