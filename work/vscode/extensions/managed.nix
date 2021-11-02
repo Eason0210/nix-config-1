7d13ecab708ef65b6e026ca2ea6fdbc979a40934
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-02 15:38:59 +0100

    Format

--- work/vscode/extensions/managed.nix
+++ work/vscode/extensions/managed.nix
@@ -3,11 +3,9 @@
 # Don't edit it manually!
 # This file should always parse as an array of attrsets.
 #
-[
-  {
-    name = "swagger-viewer";
-    publisher = "Arjun";
-    version = "3.1.2";
-    sha256 = "1cjvc99x1q5w3i2vnbxrsl5a1dr9gb3s6s9lnwn6mq5db6iz1nlm";
-  }
-]
+[{
+  name = "swagger-viewer";
+  publisher = "Arjun";
+  version = "3.1.2";
+  sha256 = "1cjvc99x1q5w3i2vnbxrsl5a1dr9gb3s6s9lnwn6mq5db6iz1nlm";
+}]
